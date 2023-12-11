5e99232dc4d7de574009b5081945aea2cf644899
lbb00 <mr@lbb00.com>
2023-12-12 00:12:31 +0800

    feat: use ImageDecoder.isTypeSupported on browser

--- src/browser.ts
+++ src/browser.ts
@@ -1,59 +1,95 @@
 import type { ImgFormat } from './constants'
-import { OPTIMAL_FORMATS_DEFAULT } from './constants'
+import { OPTIMAL_FORMATS_DEFAULT, ImgFormatMIMETypes } from './constants'
 
 const cachedSupportMap: Record<ImgFormat, boolean | undefined> = {
-  avif: undefined,
-  webp: undefined,
+	avif: undefined,
+	webp: undefined,
 }
 const loadImagePromiseMap: Record<string, Promise<HTMLImageElement>> = {}
 function loadImage(src: string, key: string) {
-  if (loadImagePromiseMap[key] === undefined) {
-    loadImagePromiseMap[key] = new Promise<HTMLImageElement>((resolve, reject) => {
-      const img = new Image()
-      img.src = src
-      img.onload = () => {
-        resolve(img)
-      }
-      img.onerror = reject
-    })
-  }
+	if (loadImagePromiseMap[key] === undefined) {
+		loadImagePromiseMap[key] = new Promise<HTMLImageElement>(
+			(resolve, reject) => {
+				const img = new Image()
+				img.src = src
+				img.onload = () => {
+					resolve(img)
+				}
+				img.onerror = reject
+			},
+		)
+	}
 
-  return loadImagePromiseMap[key]
+	return loadImagePromiseMap[key]
 }
 
 export async function isSupportAVIF() {
-  if (cachedSupportMap.avif === undefined) {
-    try {
-      await loadImage(
-        'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=',
-        'avif'
-      )
-      cachedSupportMap.avif = true
-    } catch (e) {
-      cachedSupportMap.avif = false
-    }
-  }
-  return cachedSupportMap.avif
+	if (cachedSupportMap.avif === undefined) {
+		try {
+			await loadImage(
+				'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=',
+				'avif',
+			)
+			cachedSupportMap.avif = true
+		} catch (e) {
+			cachedSupportMap.avif = false
+		}
+	}
+	return cachedSupportMap.avif
 }
 
 export async function isSupportWebP() {
-  if (cachedSupportMap.webp === undefined) {
-    try {
-      const img = await loadImage('data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==', 'webp')
-      cachedSupportMap.webp = img.width > 0 && img.height > 0
-    } catch (e) {
-      cachedSupportMap.webp = false
-    }
-  }
+	if (cachedSupportMap.webp === undefined) {
+		try {
+			const img = await loadImage(
+				'data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==',
+				'webp',
+			)
+			cachedSupportMap.webp = img.width > 0 && img.height > 0
+		} catch (e) {
+			cachedSupportMap.webp = false
+		}
+	}
 
-  return cachedSupportMap.webp
+	return cachedSupportMap.webp
 }
 
 const formatSupportMap: Record<ImgFormat, () => Promise<boolean>> = {
-  avif: isSupportAVIF,
-  webp: isSupportWebP,
+	avif: isSupportAVIF,
+	webp: isSupportWebP,
 }
-export async function getOptimalImgFormatOnBrowser(optimalFormats: ImgFormat[] = OPTIMAL_FORMATS_DEFAULT) {
-  const results = await Promise.all(optimalFormats.map((format) => formatSupportMap[format]?.()))
-  return optimalFormats[results.findIndex(Boolean)]
+
+async function getOptimalImageFormatByImageDecoder(
+	optimalFormats: ImgFormat[],
+) {
+	for (const format of optimalFormats) {
+		try {
+			if (await ImageDecoder.isTypeSupported(ImgFormatMIMETypes[format])) {
+				return format
+			}
+		} catch (e) {
+			// ignore
+		}
+	}
+}
+
+async function getOptimalImageFormatByCheckFeature(
+	optimalFormats: ImgFormat[],
+) {
+	const results = await Promise.all(
+		optimalFormats.map((format) => formatSupportMap[format]?.()),
+	)
+	return optimalFormats[results.findIndex(Boolean)]
+}
+
+export function getOptimalImgFormatOnBrowser(
+	optimalFormats: ImgFormat[] = OPTIMAL_FORMATS_DEFAULT,
+) {
+	if (
+		typeof ImageDecoder !== 'undefined' &&
+		typeof ImageDecoder.isTypeSupported === 'function'
+	) {
+		return getOptimalImageFormatByImageDecoder(optimalFormats)
+	}
+	return getOptimalImageFormatByCheckFeature(optimalFormats)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,36 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  root: '.',
-  mode: 'production',
-  build: {
-    target: 'esnext',
-    outDir: './dist',
-    minify: 'terser',
-    lib: {
-      entry: './src/index.ts',
-      formats: ['es', 'cjs', 'umd'],
-      name: 'OptimalImgFormat',
-      fileName: (format) => {
-        switch (format) {
-          case 'es':
-            return 'index.mjs'
-          case 'umd':
-            return 'index.umd.js'
-          default:
-            return 'index.js'
-        }
-      },
-    },
-  },
-  plugins: [
-    dts({
-      logLevel: 'warn',
-      rollupTypes: true,
-      include: ['src'],
-      afterBuild: () => {
-        copyFileSync('dist/index.d.ts', 'dist/index.d.mts')
-      },
-    }),
-  ],
+	root: '.',
+	mode: 'production',
+	build: {
+		target: 'esnext',
+		outDir: './dist',
+		minify: 'terser',
+		lib: {
+			entry: './src/index.ts',
+			formats: ['es', 'cjs', 'umd'],
+			name: 'OptimalImgFormat',
+			fileName: (format) => {
+				switch (format) {
+					case 'es':
+						return 'index.mjs'
+					case 'umd':
+						return 'index.umd.js'
+					default:
+						return 'index.js'
+				}
+			},
+		},
+	},
+	plugins: [
+		dts({
+			logLevel: 'warn',
+			rollupTypes: true,
+			include: ['src'],
+			afterBuild: () => {
+				copyFileSync('dist/index.d.ts', 'dist/index.d.mts')
+			},
+		}),
+	],
 })
